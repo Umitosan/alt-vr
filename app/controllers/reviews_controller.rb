d0e6dd7dffc8f152d8d283dc973140a11742c65d
Dominic Brown <yeahdom@gmail.com>
2017-07-07 17:58:25 -0700

    add delete reviews feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,11 +11,19 @@ class ReviewsController < ApplicationController
     @review = @realm.reviews.new(review_params)
     @review.user_id = @user.id
     if @review.save
-      flash[:notice] = "review added!"
+      flash[:notice] = "( review added )"
       redirect_to realm_path(@realm)
     else
       render :new
     end
+  end
+
+  def destroy
+    @realm = Realm.find(params[:realm_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "( Reveiw deleted )"
+    redirect_to realm_path(@realm)
   end
 
   private
